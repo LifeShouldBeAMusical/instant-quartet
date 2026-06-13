18d264309498e61a7560ff86c6b601dbf49e43d2
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-06-13 20:48:21 +0200

    Lint & Type-Check

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,6 @@
+import App from '@/App.vue'
+import router from '@/router/index.js'
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
 
 import { IonicVue } from '@ionic/vue'
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,9 +12,9 @@ export default defineConfig({
 		alias: {
 			'@': path.resolve(__dirname, './src')
 		}
-	},
-	test: {
-		globals: true,
-		environment: 'jsdom'
 	}
+	// test: {
+	// 	globals: true,
+	// 	environment: 'jsdom'
+	// }
 })
