e24f5b56f31e6108a569264de3ac812f2e8aab9d
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-06-14 18:05:18 +0200

    Style Level 0

--- client/src/components/SongButton.vue
+++ client/src/components/SongButton.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-import { SongFragment, VoicePart } from '@/graphql/types'
-import { useMusicStore } from '@/store/music-store'
+import { SongFragment, VoicePart } from '@/graphql/types';
+import { useMusicStore } from '@/store/music-store';
 import {
 	IonButton,
 	IonButtons,
+	IonChip,
 	IonContent,
 	IonHeader,
 	IonModal,
 	IonTitle,
 	IonToolbar
-} from '@ionic/vue'
-import { ref } from 'vue'
+} from '@ionic/vue';
+import { ref } from 'vue';
 
 const { song } = defineProps<{
 	song: SongFragment
@@ -29,14 +30,20 @@ const closeModal = () => (modalOpen.value = false)
 </script>
 
 <template>
-	<div>
-		<div>
-			<span>
+	<div class="song-button">
+		<div class="title-container">
+			<span class="title">
 				{{ song.title }}
 			</span>
 			<span>({{ song.voicing }})</span>
 			<div v-if="learnedParts">
-				<span v-for="part in learnedParts" :key="part">{{ part }}</span>
+				<ion-chip
+					v-for="part in learnedParts"
+					:key="part"
+					:class="`learned-part ${part.toLowerCase()}`"
+					outline
+					>{{ part }}</ion-chip
+				>
 			</div>
 		</div>
 		<ion-button @click="openModal">Learn</ion-button>
@@ -50,12 +57,14 @@ const closeModal = () => (modalOpen.value = false)
 				</ion-toolbar>
 			</ion-header>
 			<ion-content>
-				<ion-buttons>
+				<ion-buttons class="learn-button-container">
 					<ion-button
 						v-for="part in ['TENOR', 'LEAD', 'BARI', 'BASS']"
 						:key="part"
 						@click="() => learn(part)"
 						:disabled="learnedParts?.includes(part)"
+						:class="part.toLowerCase()"
+						fill="solid"
 					>
 						{{ part }}
 					</ion-button>
@@ -64,3 +73,46 @@ const closeModal = () => (modalOpen.value = false)
 		</ion-modal>
 	</div>
 </template>
+
+<style lang="scss">
+.song-button {
+	.title-container {
+		display: flex;
+		flex-flow: row wrap;
+		gap: 12px;
+		align-items: baseline;
+		.title {
+			font-size: 150%;
+		}
+		.tenor {
+			border-color: #9a9a00;
+		}
+		.lead {
+			border-color: blue;
+		}
+		.bari {
+			border-color: green;
+		}
+		.bass {
+			border-color: red;
+		}
+	}
+}
+.learn-button-container {
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: space-around;
+	.tenor {
+		color: #9a9a00;
+	}
+	.lead {
+		color: blue;
+	}
+	.bari {
+		color: green;
+	}
+	.bass {
+		color: red;
+	}
+}
+</style>
