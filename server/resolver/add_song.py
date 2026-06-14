c01632a31da5039924121c11a6b06f593cadff91
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-06-14 14:19:14 +0200

    Whoops

--- server/resolver/add_song.py
+++ server/resolver/add_song.py
@@ -4,14 +4,14 @@ from sqlalchemy import select
 from sqlalchemy.exc import NoResultFound
 
 from data.data_connection import get_async_session
-from model.database import SongModel
-from model.enum import LoginStatus, SuccessFailure
+from model.database import SongModel, UserSongAssociation
+from model.enum import LoginStatus, SuccessFailure,VoicePart
 from model.strawberry import LearnSongResult, LoginResult, Song, SongInput
 from resolver.authenticate import get_authenticated_user
 
 
 async def learn_song(
-    song_input: SongInput, token: str
+    song_input: SongInput, voice_part: VoicePart, token: str
 ) -> Union[LoginResult, LearnSongResult]:
     async with get_async_session() as session:
         try:
@@ -34,7 +34,7 @@ async def learn_song(
             await session.flush()
 
         if song_data is not None:
-            user_data.songs.append(song_data)
+            user_data.songs.append(            UserSongAssociation(song=song_data, voice_part=voice_part))
             await session.commit()
             return LearnSongResult(SuccessFailure.SUCCESS, Song.marshal(song_data))
 
