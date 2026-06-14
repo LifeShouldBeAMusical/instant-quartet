d55df9be6093205edd425bc2126b659927b9aed7
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-06-14 23:12:59 +0200

    Contributor Enum
    (cherry picked from commit 3f22398999db09229dd6eabc56ac15393f4122cc)

--- server/model/database/contributor_xref.py
+++ server/model/database/contributor_xref.py
@@ -1,8 +1,9 @@
-from sqlalchemy import ForeignKey, String
+from sqlalchemy import Enum, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from model.database.base import ModelBase
 from model.database.contributor import ContributorModel
+from model.enum.contribution_type import ContributionType
 
 
 class ContributorAssociation(ModelBase):
@@ -15,15 +16,15 @@ class ContributorAssociation(ModelBase):
         ForeignKey("contributor.id"), primary_key=True, nullable=False
     )
 
-    contribution_type: Mapped[str] = mapped_column(
-        "contribution_type", String, primary_key=True, nullable=False
+    contribution_type: Mapped[ContributionType] = mapped_column(
+        "contribution_type", Enum(ContributionType), primary_key=True, nullable=False
     )
     """Composer / Lyricist / Arranger"""
 
     contributor: Mapped[ContributorModel] = relationship(lazy="selectin")
 
     def __init__(
-        self, contributor: ContributorModel, contrib_type: str
+        self, contributor: ContributorModel, contrib_type: ContributionType
     ) -> "ContributorAssociation":
         self.contributor_id = contributor.id
         self.contribution_type = contrib_type
