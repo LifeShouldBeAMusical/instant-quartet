2cd638c9653a5c33cc6e222f57123c78726fd279
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-06-17 11:37:01 +0200

    Filter Voicings - Backend

--- server/model/enum/__init__.py
+++ server/model/enum/__init__.py
@@ -2,3 +2,4 @@ from model.enum.contribution_type import ContributionType
 from model.enum.login_status import LoginStatus
 from model.enum.success_failure import SuccessFailure
 from model.enum.voice_part import VoicePart
+from model.enum.voicing import Voicing
