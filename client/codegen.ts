829f2d209f4e75f198ba72c500dd06449de347d9
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-06-22 21:59:03 +0200

    Poseidon

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-	schema: 'http://localhost:8000/graphql',
+	schema: 'http://poseidon:8000/graphql',
 	documents: ['../client/src/graphql/queries/*.ts'],
 	generates: {
 		'./src/graphql/': {
