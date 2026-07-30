14326d104b4d7afbdb4a928de12a2cea202a6334
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-07-30 21:16:57 +0200

    Merge Songs

--- server/resolver/__init__.py
+++ server/resolver/__init__.py
@@ -1,6 +1,7 @@
 from resolver.add_song import learn_song
 from resolver.all_songs import fetch_all_songs
 from resolver.login import login
+from resolver.merge_song import merge_song
 from resolver.my_songs import fetch_my_songs
 from resolver.register import register_user
 from resolver.share_info import fetch_share_info
