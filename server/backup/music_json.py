19d7ae5e8cabd5dc1ba6b5090322b9b627e3aadb
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-07-30 18:32:14 +0200

    Rename

--- server/backup/music_json.py
+++ server/backup/music_json.py
@@ -6,7 +6,7 @@ from data.data_connection import get_async_session
 from model.database.song import SongModel
 
 
-async def music_json() -> JSONResponse:
+async def export_music_json() -> JSONResponse:
     async with get_async_session() as session:
         all_music = (await session.scalars(select(SongModel))).all()
         return JSONResponse(content=jsonable_encoder(all_music))
